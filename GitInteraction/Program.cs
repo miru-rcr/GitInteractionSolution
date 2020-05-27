c8d8c59c84ecb468c86e17b3f5d2df02756ead25
Miruna Rucareanu <miruna.rucareanu@endava.com>
2020-05-27 11:37:07 +0300

    added entry point code

--- GitInteraction/Program.cs
+++ GitInteraction/Program.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using GitInteraction.Git;
+using GitInteraction.GitHub;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitInteraction
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string chosenCommitId = string.Empty;
+            string destinationPath = ConfigurationManager.AppSettings["destinationFolder"];
+            string gitHubRepoOwner = ConfigurationManager.AppSettings["gitUsername"];
+            string gitHubRepoName = ConfigurationManager.AppSettings["repoName"];
+            string historyJsonPath = ConfigurationManager.AppSettings["historyJsonPath"];
+
+            GitInteractionService gitInteractionService = new GitInteractionService();
+            GitHubInteractionService gitHubInteractionService = new GitHubInteractionService();
+            string cloneUrl = gitHubInteractionService.GetRepositoryCloneUrl(gitHubRepoOwner, gitHubRepoName);
+
+            try
+            {
+                string clonedRepositoryPath = gitInteractionService.CloneRepository(cloneUrl, destinationPath);
+
+                IList<DTO.Commit> repositoryCommits = gitInteractionService.GetRepositoryCommits(clonedRepositoryPath);
+                chosenCommitId = repositoryCommits.First().CommitSha;
+                gitInteractionService.RestoreRepositoryToPreviousState(clonedRepositoryPath, chosenCommitId);
+
+                //list of branches + history
+                IList<DTO.Branch> branches = gitInteractionService.GetRepositoryRemoteBranches(clonedRepositoryPath);
+                string jsonBranches = JsonConvert.SerializeObject(branches, Formatting.Indented);
+                WriteJsonToFile(jsonBranches, historyJsonPath);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            Console.WriteLine(value: $"Clone for repository is done! Restored to: {chosenCommitId}");
+            Console.ReadLine();
+        }
+
+        private static void WriteJsonToFile(string jsonContent, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.WriteLine(jsonContent);
+                sw.Close();
+            }
         }
     }
 }
